f519a6dd2f1d093fb0712d9dc18793fda6f0f20c
Benie Giramata <122173524+IRATUZI@users.noreply.github.com>
2023-02-13 09:07:29 -0900

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,16 +1,11 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
 
-app.use(bodyParser.urlencoded({extended:true}))
-app.use(bodyParser.json());
+const app = express();
 
 
 app.get('/adding_numbers', (req, res) => {  //getting API for addition
 
- res.statusCode(200)
-
-  const numbers_array = req.body.numbers;//taking the array of numbers to be added
+  const numbers_array = req.query.numbers;//taking the array of numbers to be added
 
   let sum = 0;
 
@@ -25,9 +20,8 @@ app.get('/adding_numbers', (req, res) => {  //getting API for addition
 
 app.get('/multiplying_numbers', (req, res) => { //getting API for multiplication
 
-    res.statusCode(200)
 
-  const numbers_array = req.body.numbers;//taking te array of numbers to be mutiplied
+  const numbers_array = req.query.numbers;//taking te array of numbers to be mutiplied
 
   let product = 1;
   for ( i = 0; i < numbers_array.length; i++) {
